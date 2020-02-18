a1432bb1a2e50bddc3a8a99bf19f0c3d6898cc28
vova-uvarov <vova.uvarov@gmail.com>
2020-02-19 00:19:59 +0300

    correqt compute argumentValues for abstract methods

--- src/main/java/com/vuvarov/marketplace/Front2BackMessageLineMarkerProvider.java
+++ src/main/java/com/vuvarov/marketplace/Front2BackMessageLineMarkerProvider.java
@@ -2,6 +2,7 @@ package com.vuvarov.marketplace;
 
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeInsight.dataflow.SetUtil;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.psi.PsiElement;
@@ -9,20 +10,25 @@ import com.intellij.psi.PsiEnumConstant;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiMethodCallExpression;
+import com.intellij.psi.PsiModifier;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.PsiVariable;
 import com.intellij.psi.impl.source.tree.java.PsiMethodCallExpressionImpl;
 import com.intellij.psi.search.searches.MethodReferencesSearch;
+import com.intellij.psi.search.searches.OverridingMethodsSearch;
 import com.intellij.util.Query;
 import com.vuvarov.marketplace.util.PsiCacheUtils;
 import com.vuvarov.marketplace.util.PsiCommonUtil;
+import org.apache.commons.collections.SetUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.vuvarov.marketplace.util.PsiCommonUtil.*;
@@ -45,8 +51,8 @@ public class Front2BackMessageLineMarkerProvider extends RelatedItemLineMarkerPr
         String methodName = getCallMethodName((PsiMethodCallExpressionImpl) element);
         if (AMQUTIL_CLASS_NAME.equals(qualifierType) && SENDER_METHODS.contains(methodName)) {
 
-            String mqEntity = argumentValue((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
-            if (mqEntity != null) {
+            Set<String> mqEntity = argumentValues((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
+            if (!mqEntity.isEmpty()) {
                 Collection<? extends PsiElement> cachedElements = PsiCacheUtils.getCachedElements(element, () -> searchListeners(element));
 
                 NavigationGutterIconBuilder<PsiElement> gutterIcon = NavigationGutterIconBuilder.create(MarketPlaceIcons.SENDER)
@@ -57,8 +63,8 @@ public class Front2BackMessageLineMarkerProvider extends RelatedItemLineMarkerPr
         }
 
         if (qualifierType != null && qualifierType.startsWith(JMS_LISTENER_CLASS_NAME) && LISTENER_METHODS.contains(methodName)) {
-            String mqEntity = argumentValue((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
-            if (mqEntity != null) {
+            Set<String> mqEntity = argumentValues((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
+            if (!mqEntity.isEmpty()) {
                 Collection<? extends PsiElement> cachedElements = PsiCacheUtils.getCachedElements(element, () -> searchSenders(element));
                 NavigationGutterIconBuilder<PsiElement> gutterIcon = NavigationGutterIconBuilder.create(MarketPlaceIcons.LISTENER)
                         .setTargets(NotNullLazyValue.createValue(() -> cachedElements))
@@ -73,8 +79,8 @@ public class Front2BackMessageLineMarkerProvider extends RelatedItemLineMarkerPr
     private Collection<? extends PsiElement> searchSenders(@NotNull PsiElement element) {
         List<PsiMethod> senderMethods = findMethods(getScope(element), AMQUTIL_CLASS_NAME, SENDER_METHODS);
         List<PsiElement> resultElements = new ArrayList<>();
-        String sendMqEntity = argumentValue((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
-        String sendMqOperation = argumentValue((PsiMethodCallExpression) element, MQOPERATION_ARGUMENT_INDEX);
+        Set<String> sendMqEntity = argumentValues((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
+        Set<String> sendMqOperation = argumentValues((PsiMethodCallExpression) element, MQOPERATION_ARGUMENT_INDEX);
 
         for (PsiMethod method : senderMethods) {
             Query<PsiReference> search = MethodReferencesSearch.search(method);
@@ -90,8 +96,8 @@ public class Front2BackMessageLineMarkerProvider extends RelatedItemLineMarkerPr
     private Collection<? extends PsiElement> searchListeners(@NotNull PsiElement element) {
         List<PsiMethod> listenerMethods = findMethods(getScope(element), JMS_LISTENER_CLASS_NAME, LISTENER_METHODS);
         List<PsiElement> resultElements = new ArrayList<>();
-        String sendMqEntity = argumentValue((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
-        String sendMqOperation = argumentValue((PsiMethodCallExpression) element, MQOPERATION_ARGUMENT_INDEX);
+        Set<String> sendMqEntity = argumentValues((PsiMethodCallExpression) element, MQENTITY_ARGUMENT_INDEX);
+        Set<String> sendMqOperation = argumentValues((PsiMethodCallExpression) element, MQOPERATION_ARGUMENT_INDEX);
 
         for (PsiMethod method : listenerMethods) {
             Query<PsiReference> search = MethodReferencesSearch.search(method);
@@ -104,42 +110,51 @@ public class Front2BackMessageLineMarkerProvider extends RelatedItemLineMarkerPr
         return resultElements;
     }
 
-    private boolean isListener(String sendMqEntity, String sendMqOperation, PsiReference searchResult) {
+    private boolean isListener(Set<String> sendMqEntity, Set<String> sendMqOperation, PsiReference searchResult) {
         PsiElement searchResultElement = searchResult.getElement().getParent();
-        String mqEntity = argumentValue((PsiMethodCallExpression) searchResultElement, MQENTITY_ARGUMENT_INDEX);
-        String mqOperation = argumentValue((PsiMethodCallExpression) searchResultElement, MQOPERATION_ARGUMENT_INDEX);
-        return Objects.equals(mqEntity, sendMqEntity) && Objects.equals(mqOperation, sendMqOperation);
+        Set<String> mqEntity = argumentValues((PsiMethodCallExpression) searchResultElement, MQENTITY_ARGUMENT_INDEX);
+        Set<String> mqOperation = argumentValues((PsiMethodCallExpression) searchResultElement, MQOPERATION_ARGUMENT_INDEX);
+        return !SetUtil.intersect(mqEntity, sendMqEntity).isEmpty() && !SetUtil.intersect(mqOperation, sendMqOperation).isEmpty();
     }
 
-    private String argumentValue(PsiMethodCallExpression methodCallExpression, int index) {
+    private Set<String> argumentValues(PsiMethodCallExpression methodCallExpression, int index) {
         PsiExpression argumentExpression = methodCallExpression.getArgumentList().getExpressions()[index];
-        String expressionValue = valueFromExpression(argumentExpression);
-        if (expressionValue != null) {
+        Set<String> expressionValue = valueFromExpression(argumentExpression);
+        if (!expressionValue.isEmpty()) {
             return expressionValue;
         }
         if (argumentExpression instanceof PsiMethodCallExpression) {
             PsiMethod psiMethod = ((PsiMethodCallExpression) argumentExpression).resolveMethod();
             if (psiMethod != null) {
-                PsiExpression returnValueExpression = PsiCommonUtil.getReturnValue(psiMethod);
-                return valueFromExpression(returnValueExpression);
+                if (psiMethod.hasModifierProperty(PsiModifier.ABSTRACT)) {
+                    Collection<PsiMethod> overridinMethods = OverridingMethodsSearch.search(psiMethod).findAll();
+                    return overridinMethods.stream()
+                            .map(m -> valueFromExpression(getReturnValue(m))).filter(Objects::nonNull)
+                            .flatMap(Collection::stream)
+                            .collect(Collectors.toSet());
+
+                } else {
+                    PsiExpression returnValueExpression = PsiCommonUtil.getReturnValue(psiMethod);
+                    return valueFromExpression(returnValueExpression);
+                }
             }
         }
-        return null;
+        return Collections.emptySet();
     }
 
-    private String valueFromExpression(PsiExpression expression) {
-        if (expression == null) return null;
+    private Set<String> valueFromExpression(PsiExpression expression) {
+        if (expression == null) return Collections.emptySet();
         PsiReference reference = expression.getReference();
         if (reference != null) {
             PsiElement resolvedElement = reference.resolve();
             if (resolvedElement instanceof PsiEnumConstant) {
-                return ((PsiEnumConstant) resolvedElement).getName();
+                return Collections.singleton(((PsiEnumConstant) resolvedElement).getName());
             }
 
             if (resolvedElement instanceof PsiVariable) {
                 return valueFromExpression(((PsiVariable) resolvedElement).getInitializer());
             }
         }
-        return null;
+        return Collections.emptySet();
     }
 }
